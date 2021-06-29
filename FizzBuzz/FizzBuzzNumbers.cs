7d302567459c0bc25129c0c547aaa2395d3cb6e8
Antonia <avelidou@gmail.com>
2021-06-29 20:49:34 +0100

    Add project files.

---
+++ FizzBuzz/FizzBuzzNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class FizzBuzzNumbers
+    {
+        public void PrintNumbers()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(GetMessage(i) + "\n");
+            }
+        }
+
+        private string GetMessage(int number)
+        {
+            string message = "";
+
+            if (number % 3 == 0)
+                message = "Fizz";
+
+            if (number % 5 == 0)
+                return message += "Buzz";
+
+            return message == "" ? number.ToString() : message;
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Problem 1: FizzBuzz
+            var problemOne = new FizzBuzzNumbers();
+            problemOne.PrintNumbers();
+        }
+    }
+}
